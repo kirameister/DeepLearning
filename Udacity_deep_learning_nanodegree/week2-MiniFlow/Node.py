da0baf288b5942414bc966532f5148ac510d77b9
Akira KAKINOHANA <kira@kirameister.net>
2017-02-13 01:44:07 +0100

    Add Node.py

---
+++ Udacity_deep_learning_nanodegree/week2-MiniFlow/Node.py
@@ -0,0 +1,13 @@
+#!/usr/env python
+
+class Node(object):
+    def __init__(self, inbound_nodes=[]):
+        # Nodes from which this Node receives values
+        self.inbound_nodes = inbound_nodes
+        # Nodes to which this Node passes values
+        self.outbound_nodes = []
+        # For each inbound Node here, add this Node as an outbound Node to _that_ Node.
+        for n in self.inbound_nodes:
+            n.outbound_nodes.append(self)
+        # A calculated value
+        self.value = None
