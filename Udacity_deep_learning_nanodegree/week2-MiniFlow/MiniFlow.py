e02d5f211373dfcd75b415054cd9a03c530545ff
Akira KAKINOHANA <kira@kirameister.net>
2017-02-15 02:24:50 +0100

    :zap:

--- Udacity_deep_learning_nanodegree/week2-MiniFlow/MiniFlow.py
+++ Udacity_deep_learning_nanodegree/week2-MiniFlow/MiniFlow.py
@@ -20,3 +20,10 @@ class Node(object):
         """
         pass
 
+class Input(Node):
+    def __init__(self):
+        Node.__init__(self)
+
+    def forward(self, value=None):
+        if(value is not None):
+            self.value = value
