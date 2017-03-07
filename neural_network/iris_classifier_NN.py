71ee6e3aa9f56069a2d993382a0038c13aa21d52
Akira KAKINOHANA <kira@kirameister.net>
2017-03-07 03:26:44 +0100

    :zap:

--- neural_network/iris_classifier_NN.py
+++ neural_network/iris_classifier_NN.py
@@ -7,9 +7,11 @@ iris_raw = load_iris() # load the off-the-shelf iris data
 
 # store the Iris information onto Pandas DataFrame
 iris_data = pd.DataFrame(data = iris_raw['data'], columns = ['sepal_length'] + ['sepal_width'] + ['petal_length'] + ['petal_width']) 
-iris_class = pd.DataFrame(data = iris_raw['target'], columns = ['classes']) 
+iris_class = pd.DataFrame(data = iris_raw['target'], columns = ['class']) 
+
+iris_classes = pd.get_dummies(iris_class['class'], prefix='class')
 # split the data into training and testing partitions
-X_train, X_test, y_train, y_test = train_test_split(iris_data, iris_class, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(iris_data, iris_classes, test_size=0.2, random_state=0)
 
 '''
 sigmoid function: 
